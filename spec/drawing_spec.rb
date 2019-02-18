3d77623c497ee3d0dbb48b91492e27cb5f16361d
Naomi Katz <naykatz@gmail.com>
2019-02-18 06:07:55 +0000

    Automatically backed up by Learn

--- spec/drawing_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "drawing" do
-  it "a file called drawing exists in the public/img folder" do
-    file_names = Dir["public/img/*"]
-    expect(file_names).to include("public/img/drawing.png")
-  end
-
-
-end
